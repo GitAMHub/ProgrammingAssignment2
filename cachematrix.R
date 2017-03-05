ec7f20bbd465e9e283cf301071f041d4cb8dfe88
gitAMhub <buzono-github@yahoo.com>
2017-03-06 00:47:20 +0100

    Prueba2

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,6 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-#Esto es una prueba
-
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
