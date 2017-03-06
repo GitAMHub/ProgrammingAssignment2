8b5c725ad735e469063e894d57f77c180bca9f41
gitAMhub <buzono-github@yahoo.com>
2017-03-06 01:18:49 +0100

    Solution + exemple

--- cachematrix.R
+++ cachematrix.R
@@ -3,31 +3,51 @@
 # The first function "makeCacheMatrix"creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+        inv <- NULL
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                inv <<- NULL
         }
         get <- function() x
-        setmean <- function(mean) m <<- mean
-        getmean <- function() m
-        list(set = set, get = get,
-             setmean = setmean,
-             getmean = getmean)
+        setinverse <- function(inverse) inv <<- inverse
+        getinverse <- function() inv
+        list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
 
 ## "cacheSolve" computes the inverse of the "matrix" returned by "makeCacheMatrix". If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        m <- x$solve()
-        if(!is.null(m)) {
-                message("getting cached data")
-                return(m)
+        inv <- x$getinverse()
+        if(!is.null(inv)) {
+                message("Cache exists, getting it:")
+                return(inv)
         }
+        else
         data <- x$get()
-        m <- solve(data, ...)
-        x$setinverse(m)
-        m
+        inv <- solve(data)
+        x$setinverse(inv)
+        inv
 }
+
+# Exemple with code:
+#
+# x <- matrix(1:4,2,2)
+# mx <- makeCacheMatrix(x)
+# mx$get()
+#       [,1] [,2]
+# [1,]    1    3
+# [2,]    2    4
+
+# No cache first time
+# cacheSolve(mx)
+#       [,1] [,2]
+#  [1,]   -2  1.5
+#  [2,]    1 -0.5
+
+# Yes cache second time
+# cacheSolve(mx)
+# Cache exists, getting it:
+#       [,1] [,2]
+#  [1,]   -2  1.5
+#  [2,]    1 -0.5
+
